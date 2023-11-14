094e1e6c6fd116b8ebf16b62586ca5792769d2e6
Bo Gao <gaobobuq@gmail.com>
2023-11-14 16:39:16 +1000

    Revert "Auxiliary commit to revert individual files from 722fc930ee15646f61756e99667d8a077eec508e"
    This reverts commit 851c4192f06363829da607d446f771cda7e71078.

---
+++ src/metricCollection/SumoMetricStore.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosInstance } from 'axios'
+import { MetricStore } from './MetricStore'
+
+export const METRIC_STANDARDS_SUPPORTED_BY_SUMO = ['graphite', 'carbon2', 'prometheus'] as const
+type MetricStandardSupportedBySumo = (typeof METRIC_STANDARDS_SUPPORTED_BY_SUMO)[number]
+
+export class SumoMetricStore implements MetricStore {
+  private readonly axiosInstance: AxiosInstance
+
+  constructor(
+    private readonly endpoint: string,
+    private readonly standard: MetricStandardSupportedBySumo,
+  ) {
+    this.axiosInstance = axios.create()
+  }
+
+  async ingest(metrics: string[]): Promise<void> {
+    await this.axiosInstance.post(this.endpoint, metrics.join('\n'), {
+      headers: {
+        ['Content-Type']: `application/vnd.sumologic.${this.standard}`,
+      },
+    })
+  }
+
+  queryLatestMetric(query: string): Promise<number | undefined> {
+    return Promise.resolve(undefined);
+  }
+}
