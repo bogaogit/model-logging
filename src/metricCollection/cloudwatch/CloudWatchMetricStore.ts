8d256b9c9d463d82caf79de963f057165356db2e
Bo Gao <gaobobuq@gmail.com>
2023-11-14 16:38:49 +1000

    c

--- src/metricCollection/cloudwatch/CloudWatchMetricStore.ts
+++ src/metricCollection/cloudwatch/CloudWatchMetricStore.ts
@@ -34,50 +34,33 @@ export class CloudWatchMetricStore implements MetricStore {
     }
 
     async ingest(putMetricDataCommandInput: PutMetricDataCommandInput): Promise<void> {
-        if (!metrics) return
+        if (!putMetricDataCommandInput) return
 
-        for (const metric of metrics) {
-            const metricData: PutMetricDataCommandInput = {
-                Namespace: metric.namespace,
-                MetricData: [
-                    {
-                        MetricName: metric.metricName,
-                        Dimensions: metric.dimensions,
-                        Unit: metric.unit,
-                        Value: metric.value,
-                    },
-                ],
-            }
+        // const metricData: PutMetricDataCommandInput = {
+        //     Namespace: metric.namespace,
+        //     MetricData: [
+        //         {
+        //             MetricName: metric.metricName,
+        //             Dimensions: metric.dimensions,
+        //             Unit: metric.unit,
+        //             Value: metric.value,
+        //         },
+        //     ],
+        // }
 
-            // const metricData: PutMetricDataCommandInput = {
-            //   Namespace: "MyApp/Metrics9", // Replace "MyApp/Metrics" with your desired metric namespace
-            //   MetricData: [
-            //     {
-            //       MetricName: "MyCustomMetric", // Replace "MyCustomMetric" with your desired metric name
-            //       Dimensions: [
-            //         { Name: "Environment", Value: "Production" }, // Add any extra dimensions as required
-            //       ],
-            //       Unit: "Count",
-            //       Value: 700, // Set your desired metric value here
-            //     },
-            //   ],
-            // };
+        console.log(metricData);
+        console.log("capture----------------------");
 
-            // https://ap-southeast-2.console.aws.amazon.com/cloudwatch/home?region=ap-southeast-2#metricsV2?graph=~(metrics~(~(~'MyApp*2fMetrics~'MyCustomMetric~'Environment~'Production))~sparkline~true~view~'timeSeries~stacked~false~region~'ap-southeast-2~start~'-PT3H~end~'P0D~stat~'Average~period~10)&query=~'*7bMyApp*2fMetrics*2cEnvironment*7d
+        try {
+            const response = await this.cloudWatch.putMetricData(metricData);
 
-            console.log(metricData);
-            console.log("capture----------------------");
-
-            try {
-                const response = await this.cloudWatch.putMetricData(metricData);
-
-                console.log(response);
-                console.log("Metrics data sent successfully.");
-            } catch (error) {
-                console.error("Failed to send metrics data.", error);
-            }
-
+            console.log(response);
+            console.log("Metrics data sent successfully.");
+        } catch (error) {
+            console.error("Failed to send metrics data.", error);
         }
+
+
     }
 
     // https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Statistics-definitions.html
